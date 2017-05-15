00f39ec1a0a70aacbad95f3e3637e405a5bf84f0
Sang yeop Jung <syjung812@gmail.com>
2017-05-15 22:39:27 +0800

    update content, sidebar fixed length

--- js/app.js
+++ js/app.js
@@ -1,48 +1,34 @@
-$(function() {
-    ["en"].forEach(function (language){
-        $("#content-" + language).load("../" + language + "/home.html", function() {
-            $(".sidebar").css("height", $(".container").height() - 30);
-        });
-        $("#home-" + language).css("font-weight", "bold");
-        
-        $("#home-" + language).click(function () {
-            $("#content-" + language).load("../" + language + "/home.html", function() {
-                $(".sidebar").css("height", $(".container").height() - 30);
-            });
-            $(".sidebar a").css("font-weight", "normal");
-            $(this).css("font-weight", "bold");
-        });
-                   
-        $("#lecturers-" + language).click(function () {
-            $("#content-" + language).load("../" + language + "/lecturers.html", function() {
-                $(".sidebar").css("height", $(".container").height() - 30);
-            });
-            $(".sidebar a").css("font-weight", "normal");
-            $(this).css("font-weight", "bold");
-        });
-                   
-        $("#program-" + language).click(function () {
-            $("#content-" + language).load("../" + language + "/program.html", function() {
-                $(".sidebar").css("height", $(".container").height() - 30);
-            });
-            $(".sidebar a").css("font-weight", "normal");
-            $(this).css("font-weight", "bold");
-        });
-                   
-        $("#committee-" + language).click(function () {
-            $("#content-" + language).load("../" + language + "/committee.html", function() {
-                $(".sidebar").css("height", $(".container").height() - 30);
-            });
-            $(".sidebar a").css("font-weight", "normal");
-            $(this).css("font-weight", "bold");
-        });
-                   
-        $("#contacts-" + language).click(function () {
-            $("#content-" + language).load("../" + language + "/contacts.html", function() {
-                $(".sidebar").css("height", $(".container").height() - 30);
-            });
-            $(".sidebar a").css("font-weight", "normal");
-            $(this).css("font-weight", "bold");
-        });
-    })
-})
+$(document).ready(function () {
+    $("#content").load("en/home.html",);
+    $("#home").css("font-weight", "bold");
+    
+    $("#home").click(function () {
+        $("#content").load("en/home.html");
+        $(".sidebar a").css("font-weight", "normal");
+        $(this).css("font-weight", "bold");
+    });
+                
+    $("#lecturers").click(function () {
+        $("#content").load("en/lecturers.html");
+        $(".sidebar a").css("font-weight", "normal");
+        $(this).css("font-weight", "bold");
+    });
+                
+    $("#program").click(function () {
+        $("#content").load("en/program.html");
+        $(".sidebar a").css("font-weight", "normal");
+        $(this).css("font-weight", "bold");
+    });
+                
+    $("#committee").click(function () {
+        $("#content").load("en/committee.html");
+        $(".sidebar a").css("font-weight", "normal");
+        $(this).css("font-weight", "bold");
+    });
+                
+    $("#contacts").click(function () {
+        $("#content").load("en/contacts.html");
+        $(".sidebar a").css("font-weight", "normal");
+        $(this).css("font-weight", "bold");
+    });
+});
